0212fed6d78e549125815a1b6690a3e8d9073811
Nadjib KRIM <nadjib.krim@thalesgroup.com>
2020-11-23 18:17:22 -0600

    Partially completed Banking micro service

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET: api/<BankingController>
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return _accountService.GetAccounts();
+        }
+    }
+}
